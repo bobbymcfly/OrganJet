3dbd4e813160a5950f068c9626ca8db1c73be2f1
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-07-21 14:41:31 -0700

    introduced a pattern for blocking form submit when data has not yet loaded

--- retreive-data.js
+++ retreive-data.js
@@ -2,22 +2,33 @@ $(function(){
 
   var sample_url = "https://docs.google.com/spreadsheet/pub?key=0AsMGn_2Fl4CEdG1sQ1VtemtnZHRDT3pna1RzR0R6Q3c&output=csv";
 
-  var success = function(){
+  var buildFirst;
+  var buildSecond;
+
+  var successOne = function(){
     console.log(googleSpreadsheet);
     console.log(googleSpreadsheet.rows);
+    buildFirst = true;
+  };
+  var successTwo = function(){
+    console.log(googleSpreadsheet);
+    console.log(googleSpreadsheet.rows);
+    buildFirst = true;
   };
   var fail = function(){
     console.log("fail");
   };
 
-  var googleSpreadsheet = new GoogleSpreadsheet(sample_url, success, fail);
-  // googleSpreadsheet.url(sample_url);
-  // listLow = [];
-  // googleSpreadsheet.load(function(result) {
-  //   console.log(result);
-  //   listLow.push({ })
-  //   // $('#results').html(JSON.stringify(result).replace(/,/g,",\n"));
-  // });
+  window.isDone = function(){
+    if(buildFirst && buildSecond){
+      return true;
+    } else {
+      return false;
+    }
+
+  };
+
+  window.googleSpreadsheet = new GoogleSpreadsheet(sample_url, successOne, fail);
 
 
 
